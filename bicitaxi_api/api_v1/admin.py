1141b2ea88c1b81ca9dbdb6e13066d3a04771e06
Roberto Franco <roberto.zetinca@outlook.com>
2021-03-19 22:00:22 -0600

    Add models to admin

--- bicitaxi_api/api_v1/admin.py
+++ bicitaxi_api/api_v1/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from bicitaxi_api.api_v1.models import (
+    User,
+    Profile,
+    Location,
+    LocationZone,
+    LocationZonePoint,
+    LocationAssignation
+)
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Profile)
+admin.site.register(Location)
+admin.site.register(LocationZone)
+admin.site.register(LocationZonePoint)
+admin.site.register(LocationAssignation)
